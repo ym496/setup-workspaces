3173e8c791f6e9c81aa0151d161bcd47c69d7766
ym496 <yogendramishra2004@gmail.com>
2023-09-05 18:41:28 +0530

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+xdotool set_desktop 1 && google-chrome chat.openai.com --new-window && sleep 2
+xdotool set_desktop 2 && gnome-terminal --geometry=82x43 --window -- calcurse; gnome-terminal --geometry=82x43 --window -- pydoro && sleep 2
+xdotool set_desktop 3 && google-chrome https://mail.google.com/ --new-window && sleep 2
+xdotool set_desktop 0

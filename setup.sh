b29ee7ca2afdffd036623a88b3ecbd432aea4b0c
ym496 <yogendramishra2004@gmail.com>
2023-09-20 14:54:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,33 @@
-xdotool set_desktop 1 && google-chrome chat.openai.com --new-window && sleep 2
-xdotool set_desktop 2 && gnome-terminal --geometry=82x43 --window -- calcurse; gnome-terminal --geometry=82x43 --window -- pydoro && sleep 2
-xdotool set_desktop 3 && google-chrome https://mail.google.com/ --new-window && sleep 2
+#!/bin/bash
+
+num_options=$1
+shift
+
+declare -A options
+
+while [[ $# -gt 0 ]]; do
+  option_num=${1#-} 
+  option_val="$2"
+  
+  if [[ "$option_num" =~ ^[0-9]+$ ]]; then
+    options["$option_num"]=$option_val
+  else
+    echo "Invalid option number: $option_num" >&2
+    exit 1
+  fi
+
+  shift 2
+done
+
+for ((i = 1; i <= num_options; i++)); do
+  option_val=${options["$i"]}
+  echo "-------------------------"
+  echo "Running applications for Workspace $i: $option_val"
+  echo "-------------------------"
+  xdotool set_desktop $i
+  command="${options["$i"]}"
+  eval "$command"
+  sleep 2
+done
+
 xdotool set_desktop 0
